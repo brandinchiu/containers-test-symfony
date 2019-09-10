43179767458c39ccd28ff1c4452c6466f93e5b83
brandinchiu <brandinchiu@penguin>
2019-09-10 11:42:34 -0400

    added default route

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function index(Request $request)
+    {
+        return new Response('Hello Kubernetes. Your IP address is: ' . $request->getClientIp(), Response::HTTP_OK);
+    }
+}
